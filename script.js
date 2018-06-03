d685050a27d99c1ae5862991312356efa61c2e2f
Christian Basile <cbasile@ymail.com>
2018-06-03 00:42:36 -0400

    update text and ui/ux

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
     pgDemo = new Pg();
-    pgDemo.setPg("#pages-demo")
+    pgDemo.setPg("#pages-demo");
+    srch.setup("#search-demo", callBack);
+    $(".description>button").click(function() { $(this).parent().css('display', 'none'); })
 })
+
+function callBack(res) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            var arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+                .filter(s => s.indexOf(res) >= 0)
+                .map(s => "<div>" + s + "</div>");
+            resolve(arr);
+        }, 50);
+    });
+}
